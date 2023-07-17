70f85f6caf09106a8b161dc4e52dd286f59e1626
EldadHellerman <EldadHellerman@gmail.com>
2023-07-17 12:01:37 +0300

    -DCT_SIZE and NUMBER_OF_SAMPLES constants -disabling only adc interrupt instead of cli()

--- FFT.ino
+++ FFT.ino
@@ -1,72 +1,80 @@
 #include <FastLED_NeoPixel.h>
 #define NUM_LEDS 24
 #define DATA_PIN 8
+
+#define DCT_SIZE 128
+#define NUMBER_OF_SAMPLES DCT_SIZE
+
 FastLED_NeoPixel<NUM_LEDS, DATA_PIN, NEO_GRB> strip;
 
 auto color_purple = strip.Color(255, 0, 255);
 auto color_black = strip.Color(0, 0, 0);
 
-uint8_t buffer[2][256];
-uint8_t dct_coefficients[256];
+uint8_t buffer[2][NUMBER_OF_SAMPLES];
+uint8_t dct_coefficients[DCT_SIZE];
+// float cos_table[DCT_SIZE][NUMBER_OF_SAMPLES];
 volatile uint8_t buffer_current = 1;
 volatile uint8_t buffer_next = 0;
 volatile uint8_t buffer_index = 0;
 volatile bool run_fft = false;
+
 
 void setup(){
   // Serial.begin(115200);
   Serial.begin(1000000);
   // Serial.begin(9600);
   strip.begin();
+  init_cos_table();
   init_adc();
   DDRB |= (1<<7); //LED
   Serial.println("started");
+}
+
+void init_cos_table(){
+  for(int f=0; f<DCT_SIZE; f++){
+    for(int s=0; s<NUMBER_OF_SAMPLES; s++){
+      // cos_table[f][s] = cos((f*s * PI) / NUMBER_OF_SAMPLES);
+    }
+  }
+}
+
+float get_cos(int f, int s){
+  return cos((f*s * PI) / NUMBER_OF_SAMPLES);
+  // return cos_table[f][s];
 }
 
 void dct(){
   //discrete cosine transform. O(n^2).
   Serial.println("computing dct");
   int sum;
-  for(int f=0; f<256; f++){
+  for(int f=0; f<DCT_SIZE; f++){
     sum = 0;
-    for(int s=0; s<256; s++){
-      sum += buffer[buffer_current][s] * cos((f*s * PI) / 256);
+    for(int s=0; s<NUMBER_OF_SAMPLES; s++){
+      sum += buffer[!buffer_current][s] * get_cos(f,s);
     }
-    dct_coefficients[f] = sum / 256;
+    dct_coefficients[f] = sum / NUMBER_OF_SAMPLES;
   }
 }
 
 void print_buffer(){
   Serial.println("buffer:");
-  for(int s=0; s<256; s++){
+  for(int s=0; s<NUMBER_OF_SAMPLES; s++){
     Serial.print(buffer[buffer_current][s]);
-    Serial.print(",");
-  }
-}
-
-void output_buffer_to_leds(){
-  //let's show buffer maximum instead:
-  uint8_t max = 0;
-  for(int i=0; i<256; i++) if(buffer[buffer_current][i] > max) max = buffer[buffer_current][i];
-  int v = map(max, 0, 255, 0 ,NUM_LEDS);
-  for(int i=0; i<v; i++) strip.setPixelColor(i, color_purple);
-  for(int i=v; i<NUM_LEDS; i++) strip.setPixelColor(i, color_black);
-  strip.show();
-}
-
-void print_coefficients(){
-  Serial.println("fft coefficients:");
-  for(int f=0; f<256; f++){
-    Serial.print(dct_coefficients[f]);
     Serial.print(",");
   }
   Serial.println(";");
 }
 
-void output_coefficients_to_leds(){
-  // Serial.println("outputing coefficients to leds");
-  int samples_per_led = floor(256/NUM_LEDS); //lossing high frequnecy if divides with remainder
-  int v = map(max, 0, 255, 0 ,NUM_LEDS);
+void output_buffer_to_leds(){
+  //let's show buffer maximum instead:
+  // uint8_t max = 0;
+  // for(int i=0; i<256; i++) if(buffer[buffer_current][i] > max) max = buffer[buffer_current][i];
+  // int v = map(max, 0, 255, 0 ,NUM_LEDS);
+  // for(int i=0; i<v; i++) strip.setPixelColor(i, color_purple);
+  // for(int i=v; i<NUM_LEDS; i++) strip.setPixelColor(i, color_black);
+  // strip.show();
+
+  int samples_per_led = floor(NUMBER_OF_SAMPLES/NUM_LEDS); //lossing high frequnecy if divides with remainder
   for(int led=0; led<NUM_LEDS; led++){
     int avg = 0;
     for(int s = 0; s<samples_per_led; s++){
@@ -75,18 +83,42 @@ void output_coefficients_to_leds(){
     avg /= samples_per_led;
     strip.setPixelColor(led,strip.Color(avg, 0, avg));
   }
+  strip.show();
+}
+
+void print_coefficients(){
+  Serial.println("fft coefficients:");
+  for(int f=0; f<DCT_SIZE; f++){
+    Serial.print(dct_coefficients[f]);
+    Serial.print(",");
+  }
+  Serial.println(";");
+}
+
+void output_coefficients_to_leds(){
+  // Serial.println("outputing coefficients to leds");
+  int samples_per_led = floor(NUMBER_OF_SAMPLES/NUM_LEDS); //lossing high frequnecy if divides with remainder
+  for(int led=0; led<NUM_LEDS; led++){
+    int avg = 0;
+    for(int s = 0; s<samples_per_led; s++){
+      avg += dct_coefficients[led*samples_per_led + s];
+    }
+    avg /= samples_per_led;
+    strip.setPixelColor(led,strip.Color(avg, 0, avg));
+  }
+  strip.show();
 }
 
 void loop(){
     if(run_fft){
-      cli(); //wait to finish transform before accuiring more samples.
+      adc_disable_interrupt(); //wait to finish transform before accuiring more samples.
       // ADCSRA &= ~(1<<ADATE); //enable auto trigger which is in free running mode by default
       dct();
       print_buffer();
       print_coefficients();
       output_coefficients_to_leds();
       run_fft = false;
-      sei();
+      adc_enable_interrupt();
     }
 }
 
@@ -98,12 +130,24 @@ inline void buffer_move_to_next(){
 }
 
 ISR(ADC_vect){
-  PORTB ^= (1<<7);
-  if(++buffer_index == 0){
+  // PORTB ^= (1<<7); used to measure adc rate with oscillosope
+  buffer[buffer_current][buffer_index] = ADCH;
+  if(++buffer_index == NUMBER_OF_SAMPLES){
+    buffer_index = 0;
     run_fft = true;
     buffer_move_to_next();
   }
-  buffer[buffer_current][buffer_index] = ADCH;
+}
+
+inline void adc_enable_interrupt(){
+  // ADCSRA |= (1<< ADIE);
+  sei();
+}
+
+inline void adc_disable_interrupt(){
+  //cli() also disables serial so this has no side effects.
+  cli();
+  // ADCSRA &= ~(1<< ADIE);
 }
 
 void init_adc(){
